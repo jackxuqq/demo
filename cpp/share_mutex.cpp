01a3b4a49de1ed5eaf6e08d614c472fc4da69e56
xuzewei <xuzewei@poissonsoft.com>
2023-05-07 22:42:50 +0800

    add

--- cpp/share_mutex.cpp
+++ cpp/share_mutex.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <syncstream>
+#include <syncstream>
 #include <thread>
 #include <string>
 #include <mutex>
@@ -10,35 +10,35 @@ using namespace std;
 
 class Counter
 {
-        public:
-                Counter()
-                        :m_count(0)
-                {}
-                void Add(int i)
-                {
-                        unique_lock lock(m_mutex);
-                        m_count += i;
-                }
-                int Get()
-                {
-                        shared_lock lock(m_mutex);
-                        return m_count;
-                }
-		void Print(int i)
-		{
-                        unique_lock lock(m_mutex);
-			cout << "count = " << i << endl;
-		}
-        private:
-                int m_count;
-                shared_mutex m_mutex;
+    public:
+        Counter()
+            :m_count(0)
+        {}
+        void Add(int i)
+        {
+            unique_lock lock(m_mutex);
+            m_count += i;
+        }
+        int Get()
+        {
+            shared_lock lock(m_mutex);
+            return m_count;
+        }
+        void Print(int i)
+        {
+            unique_lock lock(m_mutex);
+            cout << "count = " << i << endl;
+        }
+    private:
+        int m_count;
+        shared_mutex m_mutex;
 };
 
 void procRead(Counter& c)
 {
-        //std::osyncstream bout(std::cout);
-      	//cout << "count=" << c.Get() << endl;
-	c.Print(c.Get());
+    std::osyncstream bout(std::cout);
+    cout << "count=" << c.Get() << endl;
+    //c.Print(c.Get());
 }
 
 void procWrite(Counter& c,int num)
