74f5de63da691551303593b858de58bf449c43b7
jackxuqq <jackxuqq@gmail.com>
2023-06-26 08:32:53 +0800

    daily comit

---
+++ cpp/const.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+
+void func1(const char** const p)
+{
+	*p = "ABC";
+}
+
+void func2(char ** p)
+{
+	*p = new char[10];
+	memset(*p, 0, 10);
+	strcpy(*p, "ABC");
+}
+
+int main()
+{
+	const char* p;
+	func1(&p);
+	//coredump below
+	//strcpy((char*)p, "abc");
+
+	char* q;
+	func2(&q);
+	strcpy(q, "abc");
+	std::cout << "q = " << q << std::endl;
+}
